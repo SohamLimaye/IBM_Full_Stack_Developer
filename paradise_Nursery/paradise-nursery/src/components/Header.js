f45d1512308db88b2674c96042b7cfd6946aecc7
SohamLimaye <soham.22111058@viit.ac.in>
2024-09-25 22:32:58 +0530

    finalizing the layout.

--- paradise_Nursery/paradise-nursery/src/components/Header.js
+++ paradise_Nursery/paradise-nursery/src/components/Header.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../context/CartContext';
 import './Header.css';  // Import the CSS file
+import cartIcon from '../assets/images/shopping_cart_icon.png';  // Import the cart icon image
 
 const Header = () => {
   const { cart } = useContext(CartContext);
@@ -13,7 +14,10 @@ const Header = () => {
         <Link to="/">Home</Link>
         <Link to="/products">Products</Link>
       </nav>
-      <Link to="/cart" className="cart-icon">Cart ({totalItems})</Link>
+      <Link to="/cart" className="cart-icon">
+        <img src={cartIcon} alt="Cart" className="cart-icon-image" /> 
+        Cart ({totalItems})
+      </Link>
     </header>
   );
 };
